a2e3cba0859558ed629a8f984d1ed1bbcf8b546e
wanderingkitty <alinaericson95@gmail.com>
2024-09-24 19:10:20 +0200

    added cartRouter get request and it works in insomnia

--- product-api/src/cartRouter.ts
+++ product-api/src/cartRouter.ts
@@ -1,58 +1,33 @@
-import "dotenv/config";
-
-import { MongoClient, Db, Collection } from "mongodb";
 import { Router, Request, Response } from "express";
+import { Collection } from "mongodb";
 import { Cart } from "./data/cart.js";
 import { logWithLocation } from "./helpers/betterConsoleLog.js";
+import { db } from "../src/data/dbConnection.js";
 
 const cartRouter = Router();
-
-const connectionString = process.env.CONNECTION_STRING;
-const dbName = process.env.MONGODB_DB_NAME;
-
-if (!connectionString) {
-	console.error("CONNECTION_STRING is not defined in environment variables");
-	process.exit(1);
-}
-
-if (!dbName) {
-	console.error("MONGODB_DB_NAME is not defined in environment variables");
-	process.exit(1);
-}
-
-const client: MongoClient = new MongoClient(connectionString);
-let db: Db;
 let collection: Collection<Cart>;
 
-async function connect() {
-	try {
-		await client.connect();
-		db = client.db(dbName);
-		collection = db.collection<Cart>("cart");
-		logWithLocation(`Connected to the database successfully.`, "success");
-		logWithLocation(
-			`Number of products: ${await collection.countDocuments()}`,
-			"success"
-		);
-	} catch (error: any) {
-		logWithLocation(`Failed to connect to the database. ${error}`, "error");
-		throw error; // Re-throw to handle in server.ts
-	}
-}
+// Initialize collection
+cartRouter.use((req, res, next) => {
+	collection = db.collection<Cart>("cart");
+	next();
+});
 
-// List all users
+// List all cart
+/* This part of the code defines a route handler for a GET request to the root path ("/") of the
+userRouter. When a GET request is made to this path, the handler function is executed
+asynchronously. */
 cartRouter.get("/", async (req: Request, res: Response) => {
 	try {
 		const cart = await collection.find().toArray();
 		res.status(200).json(cart);
 	} catch (error: any) {
-		logWithLocation(`Error fetching products: ${error.message}`, "error");
+		logWithLocation(`Error fetching cart: ${error.message}`, "error");
 		res.status(500).json({
-			message: "Error fetching products",
+			message: "Error fetching cart",
 			error: error.message,
 		});
 	}
-	await client.close();
 });
 
-export { cartRouter, connect, client };
+export { cartRouter };
